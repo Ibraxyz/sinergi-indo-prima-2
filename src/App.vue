1de5c7f2695672e356918933830db21f8aef5a8a
Ibraxyz <yulizarsofyan14@gmail.com>
2022-06-08 16:04:14 +0700

    update animated background

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,116 @@
 <template>
-  <div>
-      <h1>Sinergi Indo Prima </h1>
-  </div>
+  <Hero>
+    <template v-slot:background>
+      <AnimatedBackground :images="images" />
+    </template>
+    <template #content>
+      <div class="temp">
+        <nav>
+          <div class="container" >
+            <ul>
+              <li >Logo</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Service</li>
+            </ul>
+          </div>
+        </nav>
+        <div class="content">
+          <div class="container">
+            <div style="box-sizing: border-box; padding-top: 200px">
+              <h1 style="font-size: 100px; margin: 0px">Grow Together.</h1>
+              <p style="max-width: 650px; opacity: 0.8; font-size: 24px">
+                It is a long established fact that a reader will be distracted by the
+                readable content of a page when looking at its layout. The point of using
+                Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                as opposed to using 'Content here, content here', making it look like
+                readable English.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Hero>
+    <Hero>
+    <template v-slot:background>
+      <AnimatedBackground :images="images" />
+    </template>
+    <template #content>
+      <div class="temp">
+        <nav>
+          <div class="container" >
+            <ul>
+              <li >Logo</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Service</li>
+            </ul>
+          </div>
+        </nav>
+        <div class="content">
+          <div class="container">
+            <div style="box-sizing: border-box; padding-top: 200px">
+              <h1 style="font-size: 100px; margin: 0px">Grow Together.</h1>
+              <p style="max-width: 650px; opacity: 0.8; font-size: 24px">
+                It is a long established fact that a reader will be distracted by the
+                readable content of a page when looking at its layout. The point of using
+                Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                as opposed to using 'Content here, content here', making it look like
+                readable English.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Hero>
 </template>
 
-<script>
+<script setup>
+import "./assets/base.css";
+import AnimatedBackground from "./components/AnimatedBackground.vue";
+import Hero from "./components/Hero.vue";
+import { ref } from "vue";
+
+const images = ref(["bg1.png", "bg2.png", "bg3.png"]);
 </script>
 
 <style scoped>
+.temp {
+  color: #ffffff;
+}
 
+ul {
+  display: flex;
+  justify-content: right;
+  margin: 0px;
+  box-sizing: border-box;
+  padding: 10px;
+}
+
+ul li {
+  padding-top:10px;
+  margin: 10px 20px 10px 0px;
+  text-transform: uppercase;
+  list-style-type: none;
+  cursor: pointer;
+}
+
+ul li:first-child {
+  margin-right: auto;
+}
+
+ul li:last-child{
+  margin-right:0px;
+}
+
+.content {
+}
+
+.container {
+  box-sizing: border-box;
+  max-width: 1110px;
+  margin: auto;
+}
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="bg-wrapper">
+      <slot name="background"></slot>
+    </div>
+    <div class="overlay">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+
+.bg-wrapper {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 1;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  z-index: 2;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.8),
+    rgba(0, 0, 0, 0.1)
+  );
+}
+</style>
